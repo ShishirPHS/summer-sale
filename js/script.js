09ced632f45d6a795f31bd62edf2a8f8c81bb6c4
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-08-20 20:38:54 +0600

    if any card clicked selected items show in another div

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
 const btnApply = document.getElementById("btn-apply");
-
 btnApply.disabled = true;
 
+const btnMakePurchase = document.getElementById("btn-make-purchase");
+btnMakePurchase.disabled = true;
+
 function handleClickedCard(target) {
-    
+  const itemName = target.childNodes[7].innerText;
+
+  const selectedItemsContainer = document.getElementById("selected-items-list");
+
+  const li = document.createElement("li");
+  li.classList.add(
+    "text-[#111]",
+    "text-2xl",
+    "font-medium",
+    "leading-normal",
+    "mb-2"
+  );
+  li.innerText = itemName;
+  selectedItemsContainer.appendChild(li);
 }
